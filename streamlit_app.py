9aab5259c19a691b363f6c7b72bb633b9416e98d
tomerkoren <tomerkoren@users.noreply.github.com>
2023-06-21 00:29:16 +0300

    push

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,7 @@ gc = gspread.authorize(credentials)
 
 
 #### Read Google Sheets input ####
-st.write("Reading data from spreadsheet...")
-st.progress(0)
+pbar = st.progress(0, text="Reading data from spreadsheet...")
 
 sheet_url = st.secrets["private_gsheets_url"]
 workbook = gc.open_by_url(sheet_url)
@@ -34,7 +33,7 @@ for row in data_rows:
         exam_demands.append(demand)
 
 # st.write(list(zip(exam_names, exam_demands)))
-st.progress(20)
+pbar.progress(20)
 
 # Extract dates
 dates = []
@@ -49,7 +48,7 @@ for row in data_rows:
         dates_capacity.append(capacity)
 
 # st.write([(date, capacity) for date, capacity in zip(dates, dates_capacity)])
-st.progress(40)
+pbar.progress(40)
 
 # Extract gap constraints
 min_days_between_exams = {}
@@ -61,8 +60,8 @@ for row in data_rows:
         min_days = int(min_days)
         min_days_between_exams[(exam1, exam2)] = min_days
 
-st.write(min_days_between_exams)
-st.progress(60)
+# st.write(min_days_between_exams)
+pbar.progress(60)
 
 # Extract precedence constraints
 exam_before_exam = []
@@ -83,7 +82,7 @@ for row in data_rows:
 
 # st.write(exam_before_exam)
 # st.write(exam_before_date)
-st.progress(80)
+pbar.progress(80)
 
 # Extract prescheduled constraints
 exam_on_date = []
@@ -95,77 +94,78 @@ for row in data_rows:
         exam_on_date.append((exam, date))
 
 # st.write(exam_on_date)
-st.progress(101)
+pbar.progress(101)
 
 
 
 #### Solve scheduling problem ####
 
-from ortools.sat.python import cp_model
-from datetime import datetime
+with st.spinner(text="Solving scheduling problem..."):
+    from ortools.sat.python import cp_model
+    from datetime import datetime
 
-# Define the number of exams and the number of days
-num_exams = len(exam_names)
-horizon = len(dates)
+    # Define the number of exams and the number of days
+    num_exams = len(exam_names)
+    horizon = len(dates)
 
-# Create a CP-SAT model
-model = cp_model.CpModel()
+    # Create a CP-SAT model
+    model = cp_model.CpModel()
 
-# Create variables
-exams = [model.NewIntVar(1, horizon, f'exam_{i}') for i in range(num_exams)]
+    # Create variables
+    exams = [model.NewIntVar(1, horizon, f'exam_{i}') for i in range(num_exams)]
 
-# Add minimal gap constraints
-for (i, j), min_days in min_days_between_exams.items():
-    # Interval for each exam
-    interval_i = model.NewFixedSizeIntervalVar(exams[i], min_days, f'task_{i,j}')
-    interval_j = model.NewFixedSizeIntervalVar(exams[j], min_days, f'task_{j,i}')
-    model.AddNoOverlap([interval_i, interval_j])
-    # model.Add(exams[i] + min_days <= exams[j] or exams[j] + min_days <= exams[i])
+    # Add minimal gap constraints
+    for (i, j), min_days in min_days_between_exams.items():
+        # Interval for each exam
+        interval_i = model.NewFixedSizeIntervalVar(exams[i], min_days, f'task_{i,j}')
+        interval_j = model.NewFixedSizeIntervalVar(exams[j], min_days, f'task_{j,i}')
+        model.AddNoOverlap([interval_i, interval_j])
+        # model.Add(exams[i] + min_days <= exams[j] or exams[j] + min_days <= exams[i])
 
-# Add daily capacity constraints
-max_capacity = max(dates_capacity)
-exam_intervals = [model.NewFixedSizeIntervalVar(exams[i], 1, f'demand_{i}') for i in range(num_exams)]
-fake_intervals = [model.NewFixedSizeIntervalVar(t, 1, f'fake_demand_{t}') for t in range(horizon)]
-all_intervals = exam_intervals + fake_intervals
-all_demands = exam_demands + [max_capacity - c for c in dates_capacity]
-model.AddCumulative(all_intervals, all_demands, max_capacity)
+    # Add daily capacity constraints
+    max_capacity = max(dates_capacity)
+    exam_intervals = [model.NewFixedSizeIntervalVar(exams[i], 1, f'demand_{i}') for i in range(num_exams)]
+    fake_intervals = [model.NewFixedSizeIntervalVar(t, 1, f'fake_demand_{t}') for t in range(horizon)]
+    all_intervals = exam_intervals + fake_intervals
+    all_demands = exam_demands + [max_capacity - c for c in dates_capacity]
+    model.AddCumulative(all_intervals, all_demands, max_capacity)
 
-# Add precedence constraints
-for (i,j) in exam_before_exam:
-    model.Add(exams[i] < exams[j])
-for (i,t) in exam_before_date:
-    model.Add(exams[i] < t)
+    # Add precedence constraints
+    for (i,j) in exam_before_exam:
+        model.Add(exams[i] < exams[j])
+    for (i,t) in exam_before_date:
+        model.Add(exams[i] < t)
 
-# Add prescheduling constraints
-for (i,t) in exam_on_date:
-    model.Add(exams[i] == t)
+    # Add prescheduling constraints
+    for (i,t) in exam_on_date:
+        model.Add(exams[i] == t)
 
 
-# Define the objective
-makespan = model.NewIntVar(0, horizon, 'makespan')
-model.AddMaxEquality(makespan, exams)
-model.Minimize(makespan)
+    # Define the objective
+    makespan = model.NewIntVar(0, horizon, 'makespan')
+    model.AddMaxEquality(makespan, exams)
+    model.Minimize(makespan)
 
-# Create a solver and solve the model
-solver = cp_model.CpSolver()
-status = solver.Solve(model)
+    # Create a solver and solve the model
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
 
-# dump solution into a dictionary
-solution = {}
-if status == cp_model.OPTIMAL:
-    print('Exam schedule:')
-    for i in range(num_exams):
-        exam = exam_names[i]
-        date = dates[solver.Value(exams[i])]
-        date = datetime.strptime(date, '%d/%m/%Y').date()
-        solution[exam] = date
+    # dump solution into a dictionary
+    solution = {}
+    if status == cp_model.OPTIMAL:
+        print('Exam schedule:')
+        for i in range(num_exams):
+            exam = exam_names[i]
+            date = dates[solver.Value(exams[i])]
+            date = datetime.strptime(date, '%d/%m/%Y').date()
+            solution[exam] = date
 
-# Print the solution
-if len(solution) > 0:
-    sorted_items = sorted(solution.items(), key=lambda x:x[1])
-    for i, (exam, date) in enumerate(sorted_items):
-        datestr = date.strftime('%d/%m/%Y')
-        # datestr = date
-        st.write(f'{datestr} : {exam}')
-else:
-    st.write('No solution found')
+    # Print the solution
+    if len(solution) > 0:
+        sorted_items = sorted(solution.items(), key=lambda x:x[1])
+        for i, (exam, date) in enumerate(sorted_items):
+            datestr = date.strftime('%d/%m/%Y')
+            # datestr = date
+            st.write(f'{datestr} : {exam}')
+    else:
+        st.write('No solution found')

56e7d0caf154993b92ef235f775d965deace4220
tomerkoren <tomerkoren@users.noreply.github.com>
2023-10-19 17:51:46 +0300

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import gspread
 import re
 from google.oauth2 import service_account
 
-#### function defs ####
+#### regex helper functions ####
 def get_matching(pattern, names, index):
     return [index[name] for name in names if re.match(pattern,name)]
 
@@ -86,7 +86,7 @@ with st.spinner(text=message.capitalize() + '...'):
 
         if not (pattern1 and pattern2): continue
         pairs = get_matching_pairs(pattern1,pattern2,exam_names,exam_index)
-        st.write(f'found matching pairs for gap constraints: {pairs}')
+        # st.write(f'found matching pairs for gap constraints: {pairs}')
 
         if min_days:
             min_days = int(min_days)
@@ -109,7 +109,7 @@ with st.spinner(text=message.capitalize() + '...'):
         if not (pattern1 and pattern2): continue
         
         pairs = get_matching_pairs(pattern1,pattern2,exam_names,exam_index)
-        st.write(f'found {len(pairs)} matching pairs for precedence constraints')
+        # st.write(f'found {len(pairs)} matching pairs for precedence constraints')
 
         for (exam1, exam2) in pairs:
             exam_before_exam.append((exam1, exam2))
@@ -133,7 +133,7 @@ with st.spinner(text=message.capitalize() + '...'):
         if not (pattern and date): continue
 
         matches = get_matching(pattern,exam_names,exam_index)
-        st.write(f'found {len(matches)} matches for prescheduled constraints')
+        # st.write(f'found {len(matches)} matches for prescheduled constraints')
         date = date_index[date]
 
         for exam in matches: 

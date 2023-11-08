4f94f41ee37b1acbe57ce7e2912e58d5f015b122
tomerkoren <tomerkoren@users.noreply.github.com>
2023-11-09 01:17:06 +0200

    allowing 60min

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ st.title('Exam scheduler 2024a')
 st.write('Enter data in spreadsheet:')
 st.write(st.secrets["private_gsheets_url"])
 
-time_limit_mins = st.slider('Time limit (minutes):', min_value=0.5, max_value=10.0, value=1.0, step=0.5)
+time_limit_mins = st.slider('Time limit (minutes):', min_value=1, max_value=60, value=1, step=1)
 
 if not st.button("Process!"):
     st.stop()
